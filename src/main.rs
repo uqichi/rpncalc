9c2bc09fd5590f83da5c08c64cf2e195b36b0c86
Tamura Yukichi <tamura.yukichi@gmail.com>
2021-05-18 18:18:17 +0900

    complete

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,83 @@
+use clap::Clap;
+use std::fs::File;
+use std::io::{stdin, BufRead, BufReader};
+
+#[derive(Clap, Debug)]
+#[clap(
+    name = "My RPN Program",
+    version = "1.0.0",
+    author = "my name",
+    about = "RPN Calculator"
+)]
+
+struct Opts {
+    #[clap(short, long)]
+    verbose: bool,
+    #[clap(name = "FILE")]
+    formula_file: Option<String>,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let opts = Opts::parse();
+
+    if let Some(path) = opts.formula_file {
+        let f = File::open(path).unwrap();
+        let reader = BufReader::new(f);
+        run(reader, opts.verbose)
+    } else {
+        let stdin = stdin();
+        let reader = stdin.lock();
+        run(reader, opts.verbose)
+    }
+}
+
+fn run<R: BufRead>(reader: R, verbose: bool) {
+    let calc = RpnCalculator::new(verbose);
+    for line in reader.lines() {
+        let line = line.unwrap();
+        let ans = calc.eval(&line);
+        println!("answer: {}", ans);
+    }
+}
+
+struct RpnCalculator(bool);
+
+impl RpnCalculator {
+    pub fn new(verbose: bool) -> Self {
+        Self(verbose)
+    }
+
+    pub fn eval(&self, formula: &str) -> i32 {
+        let mut tokens = formula.split_whitespace().rev().collect::<Vec<_>>();
+        self.eval_inner(&mut tokens)
+    }
+
+    fn eval_inner(&self, tokens: &mut Vec<&str>) -> i32 {
+        let mut stack = Vec::new();
+        while let Some(token) = tokens.pop() {
+            if let Ok(v) = token.parse::<i32>() {
+                stack.push(v);
+            } else {
+                let y = stack.pop().expect("invalid syntax");
+                let x = stack.pop().expect("invalid syntax");
+                let ans = match token {
+                    "+" => x + y,
+                    "-" => x - y,
+                    "*" => x * y,
+                    "/" => x / y,
+                    "%" => x % y,
+                    _ => panic!("invalid token"),
+                };
+                stack.push(ans);
+            }
+            if self.0 {
+                println!("{:?} {:?}", tokens, stack);
+            }
+        }
+        if stack.len() == 1 {
+            stack[0]
+        } else {
+            panic!("invalid syntax")
+        }
+    }
 }
